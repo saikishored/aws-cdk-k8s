be201953f79ba8d2b916c283eccb0a2678a9c372
unknown <saikishored@ditrobiz.com>
2025-05-30 10:56:35 +0530

    publish 0.5.1-beta.4

--- example/iac.ts
+++ example/iac.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import { App, StackProps } from "aws-cdk-lib";
-import { K8sStack } from "../lib/k8s-stack";
-import { K8sClusterProps } from "../lib/types";
+import { K8sStack } from "../";
+import { K8sClusterProps } from "../";
 import { InstanceSize, SubnetType } from "aws-cdk-lib/aws-ec2";
 
 const app = new App();
